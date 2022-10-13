e2b5bfb4e5fd55cc15c2713687ade15582150282
filipkot <filipkow@wit.edu.pl>
2022-10-13 17:50:15 +0200

    Delete node_modules directory

--- node_modules/json-typescript-mapper/libs/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function isTargetType(val: any, type: "object" | "string"): boolean;
-export declare function isPrimitiveOrPrimitiveClass(obj: any): boolean;
-export declare function isArrayOrArrayClass(clazz: Function): boolean;
